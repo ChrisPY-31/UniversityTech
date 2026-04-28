5084053d0aca5f7b9d39a149e215d580478a6be2
Christian Pena <christianpeya@hotmail.com>
2026-04-28 08:18:10 -0600

    se hicieron correciones a la vista cursos

--- src/Pages/NewCourse/ActionButtons.jsx
+++ src/Pages/NewCourse/ActionButtons.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const ActionButtons = () => {
-    return (
-        <div className="flex items-center justify-end gap-4">
-            <button className="px-6 py-3 rounded-lg border border-gray-300 text-sm font-medium text-gray-700 hover:bg-gray-50 transition-colors">
-                Guardar borrador
-            </button>
-            <button className="px-6 py-3 rounded-lg bg-red-500 text-white text-sm font-bold hover:bg-red-600 transition-colors">
-                Continuar
-            </button>
-        </div>
-    )
-}
+  return (
+    <div className="flex items-center justify-end gap-4">
+      <button className="px-6 py-3 rounded-lg border border-gray-300 text-sm font-medium text-gray-700 hover:bg-gray-50 transition-colors">
+        Guardar borrador
+      </button>
+      <Link to="/cursos/curriculum" 
+      className="px-6 py-3 rounded-lg bg-red-500 text-white text-sm font-bold hover:bg-red-600 transition-colors cursor-pointer ">
+        Continuar
+      </Link>
+    </div>
+  );
+};
 
-export default ActionButtons
+export default ActionButtons;
